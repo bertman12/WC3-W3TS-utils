97f5d5cab424a9fd0288e98176e0ec1a7ce73bf8
bertman12 <eddydaniel94@gmail.com>
2025-12-24 14:11:38 -0800

    update configs

--- dist/utils/item.d.ts
+++
@@ -1,9 +0,0 @@
-import { Unit } from "w3ts";
-/**
- * Checks if the unit has an item in their item slots
- * @param u
- * @param itemTypeId
- * @returns
- */
-export declare function unitHasItem(u: Unit, itemTypeId: number): boolean;
-export declare function trig_itemRecipeSystem(): void;
