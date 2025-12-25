2c9f69bdea801a2ec739d1ec7fb6dee98b0a5314
bertman12 <eddydaniel94@gmail.com>
2025-12-24 19:45:11 -0800

    udpate gitignore

--- dist/utils/quests.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function setup_quests(): void;
-export declare function addQuest(title: string, description: string, iconPath?: string, required?: boolean): void;
--- dist/utils/timer.d.ts
+++
@@ -1,5 +0,0 @@
-import { Timer } from "w3ts";
-/**
- * @param duration milliseconds
- */
-export declare function delayedTimer(duration: number, cb: (...args: any[]) => any): Timer;
